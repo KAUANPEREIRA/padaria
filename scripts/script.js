f0c06624fd248a813291b817458556933cdff69d
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2022-06-02 13:06:33 -0300

    responsividade
    tornando o site responsivo.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,19 @@
-let dispositivo = document.getElementById('dispositivo')
+/*let dispositivo = document.getElementById('dispositivo')
 const abrir = ()=>{
     if(dispositivo.style.display!='block'){
     dispositivo.style.display='block'
     }else{
         dispositivo.style.display='none'
     }
+}*/
+
+const abrirMenu =()=>{
+    document.getElementById('menu').style.width='250px'
+}
+
+const fecharMenu=()=>{
+    document.getElementById('menu').style.width='0px'
+    document.getElementById('delicia').style.width='0px'
+    document.getElementById('contatoReis').style.width='0px'
 }
 
